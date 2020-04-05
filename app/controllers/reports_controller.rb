7ddebcedb19c93825e5fd73f416c035d95912d2a
bader <bader.mazro@gmail.com>
2020-04-05 14:57:17 +0300

    show all and show only one

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,48 @@
+class ReportsController < ApplicationController
+    
+    before_action :find_report, except: [:index, :new, :create]
+
+    def destroy
+        @report.destroy
+        redirect_to  reports_path
+    end
+
+    def edit 
+    end
+
+    def update
+    @report.update(report_params)
+    redirect_to reports_path
+    end
+
+    def show
+    end
+
+    def index
+        @reports = Report.all
+    end
+
+    def new
+        @report = Report.new
+    end
+
+    def create
+        @report = Report.new(report_params)
+        @report.save
+        redirect_to @report
+    end
+
+
+
+
+
+    private
+        def report_params
+            params.require(:report).permit(:title,:description)
+        end
+
+        def find_report
+            @report = Report.find(params[:id])
+        end
+    
+end
