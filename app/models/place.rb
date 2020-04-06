4ffba373de145696b74566f7fd43374d01fd7272
bader <bader.mazro@gmail.com>
2020-04-06 17:01:09 +0300

    creating one to on relation between places and user model and adding some validation to the place

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
+    belongs_to :user
 
+    validates_uniqueness_of :user_id, :message => "is already being used"
     validates :description,:name,:latitude,:latitude, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :reports
+  has_many :reports, dependent: :destroy
+  has_one  :place, dependent: :destroy
+  
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
